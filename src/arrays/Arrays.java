85b2a8d508a1be31116b58652603dbe86cb4f37b
LoganHenning <lrhenning13@gmail.com>
2021-03-10 18:12:53 -0600

    First commit

---
+++ src/arrays/Arrays.java
@@ -0,0 +1,16 @@
+package arrays;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		/* This calendar program should output 
+		 * all the days of the week.
+		 */
+		String[] days = {"Monday", "Tuesday", "Wednesday","Thursday",
+		              "Friday", "Saturday", "Sunday"};
+		for (int i = 0; i<7; i++) {
+			System.out.println(days[i]);
+		}
+	}
+
+}
